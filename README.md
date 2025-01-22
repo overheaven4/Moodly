73c6e8e73fa086800c73ffdf2797fb5e3572554b
JaguarKovalev <jaguarkovalev@gmail.com>
2025-01-22 20:05:59 +0300

    add .env file and remake pyproject

---
+++ README.md
@@ -0,0 +1,6 @@
+# MOODLY
+## Бот для анализа эмоционального состояния
+
+Emotions Dairy Telegram-Bot Project
+
+**Link**: @the_emotions_diary_bot
--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-TOKEN = '7788542492:AAFW60UngsnjlgRcDxF600FJ5bCcwVBat8g'
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
